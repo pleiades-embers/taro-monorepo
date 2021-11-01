2ebddf526f38995acf7652a3f78bd036c43e8319
郭凯 <935267851@qq.com>
2021-11-01 11:31:45 +0800

    配置 kernel 类的 jest debugger 代码

--- package/taro-service/src/Kernel.ts
+++ package/taro-service/src/Kernel.ts
@@ -1,11 +1,39 @@
-
 import { EventEmitter } from "events";
-import {PluginItem} from "@tarojs/taro/types/compile"
+import { PluginItem } from "@tarojs/taro/types/compile";
 
-export default class Kernel extends EventEmitter{
-    appPath:string
-    isWatch:boolean
-    isProduction:boolean
-    optsPresets:PluginItem[] | void
+interface IKernelOptions {
+  appPath: string;
+  presets?: PluginItem[];
+  plugins?: PluginItem[];
 }
 
+export default class Kernel extends EventEmitter {
+  appPath: string;
+  isWatch: boolean;
+  isProduction: boolean;
+  optsPresets: PluginItem[] | void;
+  optsPlugins: PluginItem[] | void;
+  plugins: Map<string, string>;
+  helper: any;
+  runOpts: any;
+  debugger: Function;
+
+  constructor(options: IKernelOptions) {
+    super();
+    this.debugger=(value) => {
+        console.log(value);
+    }
+    this.appPath = options.appPath || process.cwd();
+    this.optsPresets = options.presets;
+    this.optsPlugins = options.plugins;
+    this.initHelper();
+  }
+  init() {
+    console.log(1111);
+  }
+
+  initHelper() {
+    this.debugger("initHelper");
+    
+  }
+}
