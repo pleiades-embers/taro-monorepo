6d52f5f06582644a7f04ee2e911242512e0789d1
郭凯 <935267851@qq.com>
2021-11-03 17:07:20 +0800

    @babel/preset-env 最新版包含 @babel/plugin-proposal-class-properties

--- package/taro-helper/src/babelRegister.ts
+++ package/taro-helper/src/babelRegister.ts
@@ -4,19 +4,21 @@ export default function createBabelRegister({ only }) {
     //编译匹配所有条件的文件路径。
     only: Array.from(new Set([...only])),
     presets: [
-      //将最新的javascript 转换成 browser 里设置的 
+      //将最新的javascript 转换成 browser 里设置的
       require.resolve("@babel/preset-env"),
       //支持 typescript
       require.resolve("@babel/preset-typescript"),
     ],
     plugins: [
       [
+        // tc39 提案 装饰器预设
         require.resolve("@babel/plugin-proposal-decorators"),
         {
           legacy: true,
         },
       ],
-      require.resolve("@babel/plugin-proposal-class-properties"),
+      require.resolve("@babel/plugin-proposal-object-rest-spread")
     ],
+
   });
 }
